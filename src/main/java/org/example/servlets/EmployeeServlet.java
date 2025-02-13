a01ae1bf5a9aadd5f319fe8c87206a7e519c2962
KarthikRagula <karthik.ragula@wavemaker.com>
2025-02-13 11:25:48 +0530

    added employee entity

--- src/main/java/org/example/servlets/EmployeeServlet.java
+++ src/main/java/org/example/servlets/EmployeeServlet.java
@@ -1,28 +1,24 @@
 package org.example.servlets;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.entity.Employee;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-@WebServlet("/employee")
 public class EmployeeServlet extends HttpServlet {
-    private static final List<List<String>> listOfEmp = new ArrayList<>();
+    private static final List<Employee> listOfEmp = new ArrayList<>();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String name = req.getParameter("name");
-        String age = req.getParameter("age");
+        int age = Integer.parseInt(req.getParameter("age"));
         String phone = req.getParameter("phone");
-        if(name!=null && age!=null && phone !=null){
-            List<String> emp=new ArrayList<>();
-            emp.add(name);
-            emp.add(age);
-            emp.add(phone);
+        if(name!=null && phone !=null){
+            Employee emp=new Employee(name, age, phone);
             listOfEmp.add(emp);
         }
         resp.sendRedirect("employee");
@@ -35,8 +31,8 @@ public class EmployeeServlet extends HttpServlet {
         out.println("<html><head><title>Employees</title></head><body>");
         out.println("<h2>Employee List</h2>");
         out.println("<table><tr><th>Name</th><th>Age</th><th>Phone</th></tr>");
-        for (List<String> emp : listOfEmp) {
-            out.println("<tr><td>" + emp.get(0) + "</td><td>" + emp.get(1) + "</td><td>" + emp.get(2) + "</td></tr>");
+        for (Employee emp : listOfEmp) {
+            out.println("<tr><td>" + emp.getName() + "</td><td>" + emp.getAge() + "</td><td>" + emp.getPhone() + "</td></tr>");
         }
         out.println("</table>");
         out.println("<br><a href='addEmployee.html'>Add More Employees</a>");
