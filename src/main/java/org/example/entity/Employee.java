0752d3c23f147035bc798c53d8ea2e5ec3240830
KarthikRagula <karthik.ragula@wavemaker.com>
2025-02-13 13:03:50 +0530

    added put and delete methods and added idcounter

--- src/main/java/org/example/entity/Employee.java
+++ src/main/java/org/example/entity/Employee.java
@@ -1,6 +1,10 @@
 package org.example.entity;
 
+import java.util.Objects;
+
 public class Employee {
+    public static long idCounter = 1;
+    private long empId;
     private String name;
     private int age;
     private String phone;
@@ -11,7 +15,7 @@ public class Employee {
         this.phone = phone;
     }
 
-    public Employee(){
+    public Employee() {
     }
 
     public int getAge() {
@@ -36,5 +40,25 @@ public class Employee {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(long empId) {
+        this.empId = empId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return age == employee.age && Objects.equals(name, employee.name) && Objects.equals(phone, employee.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, phone);
     }
 }
--- src/main/java/org/example/servlets/EmployeeServlet.java
+++ src/main/java/org/example/servlets/EmployeeServlet.java
@@ -7,6 +7,7 @@ import org.example.entity.Employee;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class EmployeeServlet extends HttpServlet {
@@ -16,6 +17,7 @@ public class EmployeeServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Employee emp = objectMapper.readValue(req.getInputStream(), Employee.class);
+        emp.setEmpId(Employee.idCounter++);
         listOfEmp.add(emp);
         resp.setContentType("application/json");
         resp.getWriter().write(objectMapper.writeValueAsString(emp));
@@ -25,5 +27,39 @@ public class EmployeeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
         response.getWriter().write(objectMapper.writeValueAsString(listOfEmp));
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Employee updatedEmp = objectMapper.readValue(req.getInputStream(), Employee.class);
+        for (Employee emp : listOfEmp) {
+            if (emp.getEmpId() == updatedEmp.getEmpId()) {
+                emp.setName(updatedEmp.getName());
+                emp.setAge(updatedEmp.getAge());
+                emp.setPhone(updatedEmp.getPhone());
+                resp.setContentType("application/json");
+                resp.getWriter().write(objectMapper.writeValueAsString(emp));
+                return;
+            }
+        }
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        resp.getWriter().write("{\"message\": \"Employee not found\"}");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        long empId = Long.parseLong(req.getParameter("empId"));
+        Iterator<Employee> iterator = listOfEmp.iterator();
+        while (iterator.hasNext()) {
+            Employee emp = iterator.next();
+            if (emp.getEmpId() == empId) {
+                iterator.remove();
+                resp.setContentType("application/json");
+                resp.getWriter().write("{\"message\": \"Employee deleted successfully\"}");
+                return;
+            }
+        }
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        resp.getWriter().write("{\"message\": \"Employee not found\"}");
     }
 }
