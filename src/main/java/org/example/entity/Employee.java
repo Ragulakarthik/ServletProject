e72c561c39bab512c2446bfb9cfd246d2e103853
KarthikRagula <karthik.ragula@wavemaker.com>
2025-03-10 12:12:59 +0530

    added service class to do all the business logic there

--- src/main/java/org/example/entity/Employee.java
+++ src/main/java/org/example/entity/Employee.java
@@ -9,15 +9,6 @@ public class Employee {
     private int age;
     private String phone;
 
-    public Employee(String name, int age, String phone) {
-        this.name = name;
-        this.age = age;
-        this.phone = phone;
-    }
-
-    public Employee() {
-    }
-
     public int getAge() {
         return age;
     }
--- src/main/java/org/example/servlets/EmployeeServlet.java
+++ src/main/java/org/example/servlets/EmployeeServlet.java
@@ -1,65 +1,32 @@
 package org.example.servlets;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.example.entity.Employee;
+import org.example.service.InMemoryEmployeeService;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class EmployeeServlet extends HttpServlet {
-    private static final List<Employee> listOfEmp = new ArrayList<>();
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final InMemoryEmployeeService service = new InMemoryEmployeeService();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        Employee emp = objectMapper.readValue(req.getInputStream(), Employee.class);
-        emp.setEmpId(Employee.idCounter++);
-        listOfEmp.add(emp);
-        resp.setContentType("application/json");
-        resp.getWriter().write(objectMapper.writeValueAsString(emp));
+        service.addNewEmployee(req, resp);
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
-        response.getWriter().write(objectMapper.writeValueAsString(listOfEmp));
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        service.getListOfEmployees(req, resp);
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        Employee updatedEmp = objectMapper.readValue(req.getInputStream(), Employee.class);
-        for (Employee emp : listOfEmp) {
-            if (emp.getEmpId() == updatedEmp.getEmpId()) {
-                emp.setName(updatedEmp.getName());
-                emp.setAge(updatedEmp.getAge());
-                emp.setPhone(updatedEmp.getPhone());
-                resp.setContentType("application/json");
-                resp.getWriter().write(objectMapper.writeValueAsString(emp));
-                return;
-            }
-        }
-        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        resp.getWriter().write("{\"message\": \"Employee not found\"}");
+        service.updateEmployee(req, resp);
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        long empId = Long.parseLong(req.getParameter("empId"));
-        Iterator<Employee> iterator = listOfEmp.iterator();
-        while (iterator.hasNext()) {
-            Employee emp = iterator.next();
-            if (emp.getEmpId() == empId) {
-                iterator.remove();
-                resp.setContentType("application/json");
-                resp.getWriter().write("{\"message\": \"Employee deleted successfully\"}");
-                return;
-            }
-        }
-        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        resp.getWriter().write("{\"message\": \"Employee not found\"}");
+        service.deleteEmployee(req, resp);
     }
 }
